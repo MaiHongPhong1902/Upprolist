3aefe72ed9f5b43e4ba75b99b51216e184bfcbba
mai hong phong <127371458+MaiHongPhong1902@users.noreply.github.com>
2024-12-26 23:42:14 +0700

    Create TIDAL.js

---
+++ AppList/TIDAL.js
@@ -0,0 +1,19 @@
+var objc = JSON.parse($response.body);
+
+objc = {
+    "startDate": "2024-10-20T02:39:09.042+0000",
+    "validUntil": "2099-10-20T02:39:05.000+0000",
+    "status": "ACTIVE",
+    "subscription": {
+      "type": "PREMIUM_PLUS",
+      "offlineGracePeriod": 30
+    },
+    "highestSoundQuality": "HI_RES",
+    "premiumAccess": true,
+    "canGetTrial": false,
+    "paymentType": "APPLE_WALLET",
+    "paymentOverdue": false
+  }
+
+$done({ body: JSON.stringify(objc) });
+1
